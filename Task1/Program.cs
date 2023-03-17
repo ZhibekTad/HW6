ac1c9c2a3e85d4a71d2d903035e42f4643c5697b
Zhibekt Tadzhibayeva <jibektad@mail.ru>
2023-03-17 16:31:01 +0600

    Initial commit

---
+++ Task1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Пользователь вводит с клавиатуры М чисел. Посчитайте, 
+// сколько чисел больше 0 ввел пользователь
+
+Console.Write("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = arr.Count(x => x > 0);
+Console.WriteLine($"Кол-во элементов > 0: {count}");
+
